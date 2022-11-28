ae26af34a143845a10aabc737b9c7d3653fd6dcb
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2022-11-28 14:23:04 +0300

    Fix roundchange.QuorumOrder1

--- qbft/spectest/tests/roundchange/quorum_order1.go
+++ qbft/spectest/tests/roundchange/quorum_order1.go
@@ -10,7 +10,7 @@ import (
 // QuorumOrder1 tests LIVENESS where the rc quorum msgs in different order
 func QuorumOrder1() *tests.MsgProcessingSpecTest {
 	ks := testingutils.Testing4SharesSet()
-	pre := testingutils.BaseInstance(testingutils.TestingProposer(ks, qbft.FirstHeight, qbft.FirstRound))
+	pre := testingutils.BaseInstance(testingutils.TestingProposer(ks, qbft.FirstHeight, 2))
 	pre.State.Round = 2
 
 	prepareMsgs := []*qbft.SignedMessage{
@@ -63,16 +63,20 @@ func QuorumOrder1() *tests.MsgProcessingSpecTest {
 	return &tests.MsgProcessingSpecTest{
 		Name:          "round change quorum order 1",
 		Pre:           pre,
-		PostRoot:      "90f089b5314c7447d94f134c0ee06626fc975ed681739a662cd1dcf98f060ea2",
+		PostRoot:      "f239c0e0a74a65ea66f47d3302d0a8db62e25ef8adfcf10ab1ad9bb48ec2602e",
 		InputMessages: msgs,
 		OutputMessages: []*qbft.SignedMessage{
-			testingutils.SignQBFTMsg(ks.Shares[1], types.OperatorID(1), &qbft.Message{
-				MsgType:    qbft.ProposalMsgType,
-				Height:     qbft.FirstHeight,
-				Round:      2,
-				Identifier: []byte{1, 2, 3, 4},
-				Data:       testingutils.ProposalDataBytes([]byte{1, 2, 3, 4}, msgs, prepareMsgs),
-			}),
+			testingutils.SignQBFTMsg(
+				ks.Shares[testingutils.TestingProposer(ks, qbft.FirstHeight, 2)],
+				testingutils.TestingProposer(ks, qbft.FirstHeight, 2),
+				&qbft.Message{
+					MsgType:    qbft.ProposalMsgType,
+					Height:     qbft.FirstHeight,
+					Round:      2,
+					Identifier: []byte{1, 2, 3, 4},
+					Data:       testingutils.ProposalDataBytes([]byte{1, 2, 3, 4}, msgs, prepareMsgs),
+				},
+			),
 		},
 	}
 }
